835e2dcf86fa6b194f9db015fdf18ec316436127
Eduard Sysoev <krikisosedey@bk.ru>
2023-08-07 21:54:13 +0300

    add changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int even = 2;
+
+while (num >= even)
+{
+    Console.Write(even + " ");
+    even = even + 2;
+}
